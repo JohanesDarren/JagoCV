6fd63abacb0cd55f4d4cf8a42f3a70338fa0191e
Johanes <johanesdarrenyehuda@studenttelkomuniversity.ac.id>
2026-05-04 22:02:09 +0700

    started

--- push_to_github.ps1
+++
@@ -1,7 +0,0 @@
-# Git Push Script for JagoCV
-git init
-git add .
-git commit -m "Initial commit - Push seluruh aplikasi JagoCV"
-git branch -M main
-git remote add origin https://github.com/JohanesDarren/JagoCV.git
-git push -u origin main
